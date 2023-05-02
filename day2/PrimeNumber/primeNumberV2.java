5901d851ef22dfd73d470e8268ff597f3d32e97f
jeanjadedg <130419828+jeanjadedg@users.noreply.github.com>
2023-05-02 13:57:22 +0800

    v2 code with timer with different methods

---
+++ day2/PrimeNumber/primeNumberV2.java
@@ -0,0 +1,56 @@
+public class primeNumberV2 {
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+
+        System.out.println(isPrime2(31));
+
+        long endTime = System.nanoTime();
+        System.out.println("Duration: " + (endTime - startTime));
+    }
+
+    public static boolean isPrime(long n) {
+        if (n < 2) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        long sqrtN = (long) Math.sqrt(n) + 1;
+
+        for (long i = 6L; i <= sqrtN; i += 6) {
+            if (n % (i - 1) == 0 || n % (i + 1) == 0) return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isPrime1(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        for (int i = 2; i <= Math.sqrt(n) + 1; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static boolean isPrime2(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
+        for (int i = 3; i <= Math.sqrt(n) + 1; i = i + 2) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
