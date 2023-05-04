49562c27c5b01c1332986c1c683e2ea1b1e36c2d
jeanjadedg <130419828+jeanjadedg@users.noreply.github.com>
2023-05-04 20:31:06 +0800

    Uploaded OddEven.java

--- day3/OddEven.java
+++ day3/OddEven.java
@@ -1,1 +1,40 @@
+import java.util.Scanner;
 
+class OddEven {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome! This is the odd/even number counter.");
+        System.out.println();
+        oddEven();
+
+    }
+
+    public static void oddEven() {
+
+        // GETS THE ARRAY LENGTH AND THE ELEMENTS OF THE ARRAY
+        Scanner read = new Scanner(System.in);
+        System.out.print("Please enter how many numbers you want in the list: ");
+        int length = read.nextInt();
+        int [] myArray = new int[length];
+        System.out.println("Enter the elements of the array:");
+        for(int i=0; i<length; i++ ) {
+            myArray[i] = read.nextInt();
+        }
+
+        int evenCounter = 0;
+        int oddCounter = 0;
+
+        // LOOPS THE ELEMENTS OF THE ARRAY TO CHECK IF ODD OR EVEN
+        for (int j = 0; j < length; j++) {
+            if (myArray[j] % 2 == 0) {
+                evenCounter++;
+            } else {
+                oddCounter++;
+            }
+        }
+
+        System.out.println("There are " + evenCounter + " even numbers.");
+        System.out.println("There are " + oddCounter + " odd numbers.");
+    }
+}
