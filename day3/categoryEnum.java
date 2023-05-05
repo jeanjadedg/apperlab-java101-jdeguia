ee92f20150d14a29194b3522860c03b846f38461
jeanjadedg <130419828+jeanjadedg@users.noreply.github.com>
2023-05-05 12:01:22 +0800

    uploaded activity CategoryEnum.java

---
+++ day3/categoryEnum.java
@@ -0,0 +1,28 @@
+public class categoryEnum {
+
+    public static void main(String[] args) {
+
+        categoryEnumerator();
+    }
+
+    public static void categoryEnumerator() {
+        category Category = category.WOMEN;
+        double men = 13.57;
+        double women = 20.45;
+        double kid= 45.6;
+        double grandma = 56.67;
+        double grandpa= 3.56;
+
+        switch (Category) {
+            case MEN: System.out.println(men); break;
+            case WOMEN: System.out.println(women); break;
+            case KID: System.out.println(kid); break;
+            case GRANDMA: System.out.println(grandma); break;
+            case GRANDPA: System.out.println(grandpa); break;
+        }
+    }
+
+    public enum category {
+        MEN, WOMEN, KID, GRANDMA, GRANDPA
+    }
+}
